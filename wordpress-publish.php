030295a4f0ed2363ab292e010d2fb7d561c54289
Martijn Gastkemper <martijn@grrr.nl>
2024-03-06 19:57:14 +0100

    Bump version

--- wordpress-publish.php
+++ wordpress-publish.php
@@ -6,7 +6,7 @@
  * Plugin Name: WordPress Publish
  * Description: Allow publishing Next.js frontend from WordPress
  * Author:      Ramiro Hammen <ramiro@grrr.nl>
- * Version:     0.1.0
+ * Version:     0.2.0
  */
 
 use Grrr\WordpressPublish\Plugin;
