dedcf7fcd8238b194fc814f751fe40a081f0ead1
Ramiro Hammen <ramiro.hammen@norday.nl>
2025-08-27 14:35:51 +0200

    Do not load text domain before before init
    This will remove a notice about loading the text domain incorrectly
    https://stackoverflow.com/questions/79198701/notice-function-load-textdomain-just-in-time-was-called-incorrectly

--- src/Plugin.php
+++ src/Plugin.php
@@ -10,7 +10,11 @@ class Plugin
             if (!function_exists("get_plugin_data")) {
                 require_once ABSPATH . "wp-admin/includes/plugin.php";
             }
-            $plugin_data = get_plugin_data(GRRR_WORDPRESS_PUBLISH_PLUGIN_FILE);
+            $plugin_data = get_plugin_data(
+                plugin_file: GRRR_WORDPRESS_PUBLISH_PLUGIN_FILE,
+                markup: true,
+                translate: false
+            );
             $version = $plugin_data["Version"];
         } else {
             $version = "not-available";
